df391036f26947297c71ba5813ae412b98b277a2
Josh Courtney <joshrcourtney@gmail.com>
2021-05-16 12:38:08 -0500

    Update project

--- xylophone/ViewController.swift
+++ xylophone/ViewController.swift
@@ -6,14 +6,45 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func keyTapped(_ sender: UIButton) {
+        playSound(button: sender)
+        dim(button: sender)
+    }
+    
+    func playSound(button: UIButton) {
+        guard let url = Bundle.main.url(forResource: button.titleLabel?.text, withExtension: "wav") else {
+            return
+        }
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            player = try AVAudioPlayer(contentsOf: url)
+
+            guard let player = player else { return }
+
+            player.play()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func dim(button: UIButton) {
+        button.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            button.alpha = 1.0
+        }
     }
 
-
 }
-
